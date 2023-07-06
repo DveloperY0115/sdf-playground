9886b8a5aa236b7484407bec9ee825c8d788eecb
Seungwoo Yoo <dreamy1534@gmail.com>
2023-07-06 10:45:00 +0900

    Add function to make video from images

--- src/utils/images.py
+++ src/utils/images.py
@@ -4,4 +4,33 @@ images.py
 A collection of utilities for image processing.
 """
 
+from pathlib import Path
 
+from jaxtyping import Int, jaxtyped
+import imageio
+import numpy as np
+from numpy import ndarray
+from typeguard import typechecked
+
+
+@jaxtyped
+@typechecked
+def create_video_from_images(
+    images: Int[ndarray, "num_image height width channels"],
+    out_file: Path,
+) -> None:
+    """Creates a video from a sequence of images"""
+
+    images = images.astype(np.uint8)
+
+    writer = imageio.get_writer(
+        str(out_file),
+        format="FFMPEG",
+        mode="I",
+        fps=24,
+        macro_block_size=1,
+    )
+
+    for image in images:
+        writer.append_data(image)
+    writer.close()
