822dcd686cad1418772affd4b3e520bf35cc52fd
Seungwoo Yoo <dreamy1534@gmail.com>
2023-07-06 10:46:00 +0900

    Add function to make poses for visualization

--- src/cameras/utils.py
+++ src/cameras/utils.py
@@ -4,6 +4,7 @@ utils.py
 A set of utilities related to cameras.
 """
 
+import numpy as np
 import torch
 
 from jaxtyping import Float, jaxtyped
@@ -44,3 +45,65 @@ def compute_lookat_matrix(
     camera_to_world = torch.cat([camera_to_world, camera_position[:, None]], dim=1)
 
     return camera_to_world
+
+@jaxtyped
+@typechecked
+def convert_spherical_to_cartesian(
+    radius: float,
+    azimuth: float,
+    elevation: float,
+) -> Float[Tensor, "3"]:
+    """
+    Converts a point in spherical coordinates to cartesian coordinates.
+
+    Args:
+        radius: The radius of the sphere.
+        azimuth: The azimuth angle in radian.
+        elevation: The elevation angle in radian.
+    
+    Returns:
+        The Cartesian coordinate (x, y, z) of the point specified by
+        the given spherical coordinates.
+    """
+
+    # check arguments
+    assert radius > 0.0, f"{radius:.3f}"
+    assert 0 <= azimuth <= 2 * np.pi, f"{azimuth:.3f}"
+    assert -np.pi / 2 <= elevation <= np.pi / 2, f"{elevation:.3f}"
+
+    theta: float = np.pi / 2 - elevation
+    phi: float = azimuth
+
+    x = radius * np.sin(theta) * np.sin(phi)
+    y = radius * np.cos(theta)
+    z = radius * np.sin(theta) * np.cos(phi)
+    position = torch.tensor([x, y, z])
+
+    return position
+
+@jaxtyped
+@typechecked
+def sample_trajectory_along_upper_hemisphere(
+    radius: float,
+    elevation: float,
+    num_step: int,
+) -> Float[Tensor, "num_step 3"]:
+    """
+    Samples camera positions along the upper hemisphere of a sphere.
+
+    Args:
+        radius: The radius of the sphere.
+        elevation: The elevation angle in radian.
+        num_step: The number of azimuth steps to sample.
+    """
+
+    # sample azimuth values
+    azimuths = torch.linspace(0, 2 * np.pi, num_step + 1)[:-1]
+
+    # compute Cartesian coordinates at each sample point
+    positions = torch.zeros((num_step, 3))
+    for index, azimuth in enumerate(azimuths):
+        position = convert_spherical_to_cartesian(radius, azimuth.item(), elevation)
+        positions[index] = position
+
+    return positions
