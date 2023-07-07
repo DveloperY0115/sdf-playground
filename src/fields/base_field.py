6fe3e65388b130606891291f06f61b2e04e23d30
Seungwoo Yoo <dreamy1534@gmail.com>
2023-07-07 21:16:42 +0900

    Add file 'base_field.py'

---
+++ src/fields/base_field.py
@@ -0,0 +1,54 @@
+"""
+base_field.py
+
+The base class for field representations.
+"""
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from typing import Type
+
+from jaxtyping import Shaped
+from torch import Tensor
+
+from src.configs.base_config import InstantiateConfig
+
+
+@dataclass
+class FieldConfig(InstantiateConfig):
+    """The configuration of a field"""
+
+    _target: Type = field(default_factory=lambda: Field)
+
+
+class Field(ABC):
+    """The base class of fields"""
+
+    config: FieldConfig
+
+    def __init__(
+        self,
+        config: FieldConfig,
+    ) -> None:
+        self.config = config
+
+    @abstractmethod
+    def evaluate_density(
+        self,
+        coords: Shaped[Tensor, "num_point 3"],
+    ) -> Shaped[Tensor, "num_point"]:
+        """Evaluates the density function parameterized by SDF at the given points."""
+
+    @abstractmethod
+    def evaluate_sdf(
+        self,
+        coords: Shaped[Tensor, "num_point 3"],
+    ) -> Shaped[Tensor, "num_point"]:
+        """Evaluates the signed distance function at the given points."""
+
+    @abstractmethod
+    def evaluate_sdf_gradient(
+        self,
+        coords: Shaped[Tensor, "num_point 3"],
+    ) -> Shaped[Tensor, "num_point 3"]:
+        """Evaluates the gradient of the signed distance function at the given points."""
